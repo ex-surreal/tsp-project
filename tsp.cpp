92303a3b9f912bcdfaa0ff075ba24c79d03434c4
Abdul Mumit <mamumit07@gmail.com>
2015-05-23 15:26:27 +0600

    before submission without triangular inequality

--- tsp.cpp
+++ tsp.cpp
@@ -22,7 +22,7 @@ int main () {
 	cout << "<?php\necho json_encode(array(\n\t'data' => ";
 	cout << "[" << endl;
 	cout << "[\"Input Size\", \"Exponential Method\", \"Branch and Bound\", \"2-Approximation\"]," << endl;
-	vector <double> input[3];
+	vector <int> input[3];
 	while (cin >> n) {
 		vector <vector <int> > mat(n, vector<int>(n, 0));
 		for (int i = 0; i < n; ++i) {
@@ -39,7 +39,7 @@ int main () {
 		} else {
 			cout << ", null";
 		}
-		if (n < 16) {
+		if (n < 14) {
 			pair <int, int> bnb = tsp.solveBnB();
 			cout << "," << getMiliSecond(bnb.y);
 			input[0].push_back(n);
@@ -49,24 +49,39 @@ int main () {
 		}
 		pair <int, int> apx = tsp.solve2Apx();
 		cout << "," << getMiliSecond(apx.y);
-		if (n < 16) {
+		if (n < 14) {
 			input[2].push_back(apx.x);
 		}
 		cout << "]," << endl;
 	}
 	cout << "]));" << endl;
+
 	fclose(stdout);
-	freopen("/Applications/XAMPP/xamppfiles/htdocs/tsp-chart/apx-ratio.php", "w", stdout);
+	freopen("/Applications/XAMPP/xamppfiles/htdocs/tsp-chart/solution.php", "w", stdout);
 	cout << "<?php\necho json_encode(array(\n\t'data' => ";
 	cout << "[" << endl;
 	cout << "[\"Input Size\", \"Exact Solution\", \"2-Approximation\"]," << endl;
 	for (int i = 0; i < int(input[0].size()); ++i) {
 		cout << "[";
-		cout << int(input[0][i]);
+		cout << input[0][i];
 		cout << ", ";
 		cout << input[1][i];
 		cout << ", ";
 		cout << input[2][i];
+		cout << "]," << endl;
+	}
+	cout << "]));" << endl;
+
+	fclose(stdout);
+	freopen("/Applications/XAMPP/xamppfiles/htdocs/tsp-chart/apx-ratio.php", "w", stdout);
+	cout << "<?php\necho json_encode(array(\n\t'data' => ";
+	cout << "[" << endl;
+	cout << "[\"Input Size\", \"Approximation Ratio\"]," << endl;
+	for (int i = 0; i < int(input[0].size()); ++i) {
+		cout << "[";
+		cout << input[0][i];
+		cout << ", ";
+		cout << 1.0 * input[2][i] / input[1][i];
 		cout << "]," << endl;
 	}
 	cout << "]));" << endl;
